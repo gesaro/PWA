110438915b01b777eb872dd8448f3f9aaa4f4f47
gesaro <47378098+gesaro@users.noreply.github.com>
2019-03-05 22:43:15 -0600

    Add files via upload

--- tabla.php
+++ tabla.php
@@ -7,3 +7,20 @@
 			parent::__construct();
 			$this->table='--tabla--';
 			$this->cols=[--cols--];
+		}
+		public function find(){
+			return parent::find();
+		}
+		public function findcond($cols,$cond){
+			return parent::findcond($cols,$cond);
+		}
+		public function add($values){
+			parent::add($values);
+		}
+		public function edit($cols,$cond){
+			parent::edit($cols,$cond);
+		}
+		public function remove($cond){
+			parent::remove($cond);
+		}
+	}
